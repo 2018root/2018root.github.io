1bff081a9e15e693de018e9961a558a42f2a1a41
1azygom <gomtangi27@gmail.com>
2018-05-26 14:31:22 +0900

    180526

---
+++ js/project.js
@@ -0,0 +1,14 @@
+$('.project1').click(function () {
+  event.preventDefault();
+  $('#project1-content').modal(options)
+});
+
+/*
+$('.deleteMeetingButton').click(function () {
+    $('#popupContent').fadeOut('slow');
+$('#deleteMeetingConfirmDeleted').fadeIn('slow');
+ $('#overlay').delay(1300).fadeOut('slow');
+$('#popupBox').delay(1300).fadeOut('slow');
+    $('#deleteMeetingConfirmDeleted').fadeOut('slow');
+});
+*/
